398bf79157388ac6f02f65a5f34c3465fbc2ae14
FIlip Volner <filipv@kentico.com>
2021-05-15 15:46:39 +0200

    convert mvcutils library

---
+++ MvcUtils/ByteToHexConvertTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using System.Linq;
+
+namespace MvcUtils
+{
+    public class ByteToHexConvertTagHelper : TagHelper
+    {
+        public byte[] ByteArray { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            output.Content.SetHtmlContent(new HtmlString(string.Concat(ByteArray.Select(b => b.ToString("X2")))));
+        }
+    }
+}
